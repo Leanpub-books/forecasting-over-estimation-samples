9b09a346a917db76a2742960217717057c864d9e
Ralf Westphal <info@ralfw.de>
2021-11-06 13:28:50 +0200

    moved cycle time calculation into aggregate funktion

--- src/multiissueforecast-singledev/main.ts
+++ src/multiissueforecast-singledev/main.ts
@@ -5,18 +5,17 @@
 import {Lazy} from 'https://deno.land/x/lazy@v1.7.3/lib/mod.ts';
 import { parse } from "https://deno.land/std/flags/mod.ts"
 
-import {LoadHistory} from "../modules/HistoryReader.ts";
+import {HistoricalRecord, LoadHistory} from "../modules/HistoryReader.ts";
 import {CalculateForecast} from "../modules/Forecasting.ts";
 import {Plot} from "../modules/ForecastAsciiBarCharts.ts";
 import {Simulate} from "../modules/MonteCarloSimulation.ts";
 
 
 const args = parseCommandline()
-const history = LoadHistory(args.f);
 
-const historicalCycletimes = Lazy.from(history).select(x => x.CycleTimeDays).toArray();
-const forecastingValues = Simulate<number>(historicalCycletimes, args.s, args.n,
-    values  => { return Lazy.from(values).sum(); });
+const history = LoadHistory(args.f);
+const forecastingValues = Simulate<HistoricalRecord>(history, args.s, args.n,
+    values  => { return Lazy.from(values).select(x => x.CycleTimeDays).sum(); });
 const forecast = CalculateForecast(forecastingValues);
 
 Plot(forecast)
